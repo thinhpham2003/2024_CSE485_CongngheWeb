56edde452793d7a3b29426e7f1567687742c38de
unknown <nguyenthuhang26032003@gmail.com>
2024-02-28 09:57:29 +0700

    ThucHanh2

--- project13/index.php
+++ project13/index.php
@@ -16,13 +16,12 @@ $courses = [
     [
         'title' => 'Học viên quốc tế',
         'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ
-Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên
-quốc tế.',
+        Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên
+        quốc tế.',
         'fee' => '15.000.000 VND',
         'start_date' => '2/2/24',
         'duration' => '2 - 2.5 năm'
     ],
-    // Thêm các khóa học khác vào đây
 ];
 $courses = [
     [
--- project21/index.php
+++ project21/index.php
@@ -9,82 +9,89 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-    $products = [
-        [
-            "id" => 1,
-            "name" => "T-Shirt",
-            "description" => "A comfortable and stylish T-Shirt",
-            "price" => 15.99,
-            "image" =>'<img class="images" src="img/bag.png " alt="Mô tả hình ảnh">'
-        ],
-        [
-            "id" => 2,
-            "name" => "Jean",
-            "price" => 23,
-            "image" =>'<img class="images" src="img/stylo.png " alt="Mô tả hình ảnh">'
-        ],
-        [
-            "id" => 3,
-            "name" => "Jean",
-            "price" => 23,
-            "image" =>'<img class="images" src="img/chari.png " alt="Mô tả hình ảnh">'
-        ],
-        [
-            "id" => 4,
-            "name" => "Jean",
-            "price" => 23,
-            "image" =>'<img class="images" src="img/set.png " alt="Mô tả hình ảnh">'
-        ],
-        [
-            "id" => 5,
-            "name" => "Jean",
-            "price" => 23,
-            "image" =>'<img class="images" src="img/bag.png " alt="Mô tả hình ảnh">'
-        ],
-        [
-            "id" => 6,
-            "name" => "Jean",
-            "price" => 23,
-            "image" =>'<img class="images" src="img/bag.png " alt="Mô tả hình ảnh">'
-        ],
+<?php
+$products = [
+    [
+        "id" => 1,
+        "name" => "T-Shirt",
+        "description" => "A comfortable and stylish T-Shirt",
+        "price" => 15.99,
+        "image" =>'<img class="images" src="img/bag.png " alt="Mô tả hình ảnh">'
+    ],
+    [
+        "id" => 2,
+        "name" => "Jean",
+        "description" => "A comfortable and stylish T-Shirt",
+        "price" => 24,
+        "image" =>'<img class="images" src="img/stylo.png " alt="Mô tả hình ảnh">'
+    ],
+    [
+        "id" => 3,
+        "name" => "Jean",
+        "description" => "A comfortable and stylish T-Shirt",
+        "price" => 25,
+        "image" =>'<img class="images" src="img/chari.png " alt="Mô tả hình ảnh">'
+    ],
+    [
+        "id" => 4,
+        "name" => "Jean",
+        "description" => "A comfortable and stylish T-Shirt",
+        "price" => 26,
+        "image" =>'<img class="images" src="img/set.png " alt="Mô tả hình ảnh">'
+    ],
+    [
+        "id" => 5,
+        "name" => "Jean",
+        "description" => "A comfortable and stylish T-Shirt",
+        "price" => 27,
+        "image" =>'<img class="images" src="img/bag.png " alt="Mô tả hình ảnh">'
+    ],
+    [
+        "id" => 6,
+        "name" => "Jean",
+        "description" => "A comfortable and stylish T-Shirt",
+        "price" => 28,
+        "image" =>'<img class="images" src="img/bag.png " alt="Mô tả hình ảnh">'
+    ],
 
-    ];
-    $itemsPerPage = 4;
-    $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
-    $totalPages = ceil(count($products) / $itemsPerPage);
-    $currentPageItems =
-        array_slice($products, ($currentPage - 1) * $itemsPerPage, $itemsPerPage);
-    ?>
+];
+$itemsPerPage = 4;
+$currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+$totalPages = ceil(count($products) / $itemsPerPage);
+$currentPageItems =
+    array_slice($products, ($currentPage - 1) * $itemsPerPage, $itemsPerPage);
+?>
 
-    <div class="product-list">
-        <?php foreach ($currentPageItems as $product): ?>
-            <div class="product">
-                <?php
-                echo $product['image'];
-                echo '<p>' .$product['name']. '</p>';
-                echo '<p>' .$product['price']. '</p>';
-                ?>
-            </div>
-        <?php endforeach; ?>
-    </div>
-    <div class="pagination">
-        <?php if ($currentPage > 1): ?>
-            <a href="?page=<?php echo $currentPage - 1; ?>">Previous</a>
+<div class="product-list">
+    <?php foreach ($currentPageItems as $product): ?>
+        <div class="product">
+            <?php
+            echo $product['image'];
+            echo '<p>' .$product['name']. '</p>';
+            echo '<p>' .$product['price']. '</p>';
+            echo '<p>' .$product['description']. '</p>';
+            ?>
+        </div>
+    <?php endforeach; ?>
+</div>
+<div class="pagination">
+    <?php if ($currentPage > 1): ?>
+        <a href="?page=<?php echo $currentPage - 1; ?>">Previous</a>
+    <?php endif; ?>
+    <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+        <?php if ($i == $currentPage): ?>
+            <span class="active"><?php echo $i; ?></span>
+        <?php else: ?>
+            <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
         <?php endif; ?>
-        <?php for ($i = 1; $i <= $totalPages; $i++): ?>
-            <?php if ($i == $currentPage): ?>
-                <span class="active"><?php echo $i; ?></span>
-            <?php else: ?>
-                <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-            <?php endif; ?>
-        <?php endfor; ?>
-        <?php if ($currentPage < $totalPages): ?>
-            <a href="?page=<?php echo $currentPage + 1; ?>">Next</a>
-        <?php endif; ?>
-    </div>
+    <?php endfor; ?>
+    <?php if ($currentPage < $totalPages): ?>
+        <a href="?page=<?php echo $currentPage + 1; ?>">Next</a>
+    <?php endif; ?>
+</div>
 </body>
 </html>
+
 
 
 
